c537bf0ae00245afc84ed4423688f3f8a6216270
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2024-02-10 23:04:33 +0100

    fix main pkg json

--- src/lib/divisionsPath.ts
+++ src/lib/divisionsPath.ts
@@ -1,34 +1,53 @@
 import fs from "node:fs/promises";
 import { packageDirectory } from "pkg-dir";
+import cp from "node:child_process";
+import util from "node:util";
+
+export async function getGitRepositoryPath() {
+  const exec = util.promisify(cp.exec);
+
+  async function getGitRepositoryPath() {
+    let repositoryPath = "";
+    try {
+      const { stdout } = await exec("git rev-parse --show-toplevel");
+      repositoryPath = stdout.trim();
+    } catch (error) {
+      console.error(error);
+    }
+    return repositoryPath;
+  }
+
+  getGitRepositoryPath();
+}
 
 export async function getDivisionsLibPkgJson() {
-	const divisionsLibDirectory = await getDivisionsLibDirectory();
-	const pkgJsonPath = `${divisionsLibDirectory}/package.json`;
-	const pkgJson = await fs.readFile(pkgJsonPath, "utf-8");
-	return JSON.parse(pkgJson);
+  const divisionsLibDirectory = await getDivisionsLibDirectory();
+  const pkgJsonPath = `${divisionsLibDirectory}/package.json`;
+  const pkgJson = await fs.readFile(pkgJsonPath, "utf-8");
+  return JSON.parse(pkgJson);
 }
 
 export async function getProjectDirectoryPkgJson() {
-	const projectDirectory = await getProjectDirectory();
-	const pkgJsonPath = `${projectDirectory}/package.json`;
-	const pkgJson = await fs.readFile(pkgJsonPath, "utf-8");
-	return JSON.parse(pkgJson);
+  const projectDirectory = await getProjectDirectory();
+  const pkgJsonPath = `${projectDirectory}/package.json`;
+  const pkgJson = await fs.readFile(pkgJsonPath, "utf-8");
+  return JSON.parse(pkgJson);
 }
 
 export async function getDivisionsLibDirectory() {
-	const pkgDirPath = await packageDirectory({
-		cwd: import.meta.dirname,
-	});
-	return pkgDirPath;
+  const pkgDirPath = await packageDirectory({
+    cwd: import.meta.dirname,
+  });
+  return pkgDirPath;
 }
 
 export async function getProjectDirectory() {
-	const pkgDirPath = await packageDirectory();
-	return pkgDirPath;
+  const pkgDirPath = await packageDirectory();
+  return pkgDirPath;
 }
 
 export async function getDivisionsDirectory() {
-	const pkgDirPath = await packageDirectory();
-	const divisionsDirPath = `${pkgDirPath}/divisions`;
-	return divisionsDirPath;
+  const pkgDirPath = await packageDirectory();
+  const divisionsDirPath = `${pkgDirPath}/divisions`;
+  return divisionsDirPath;
 }
